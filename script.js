2a20c6e3402714eb365fb78545493aefd10d4612
beslerpatryk <beslerpatryk@gmail.com>
2021-11-16 09:05:44 +0800

    feat: refactor code

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const timerEl = document.querySelector(".timer-container")
+const pauseBtn = document.getElementById("pause")
+const resumeBtn = document.getElementById("resume")
 
 const settings = {
     //in minutes
@@ -9,14 +11,17 @@ const settings = {
 }
 
 const timer = {
-    pad: function(number) {
+    status: "run",
+
+    pad(number) {
         return number < 10 ? `0${number.toString()}` : number
     },
 
-    count: function(time){
+    count(time, seconds = 59){
         let minutesRemaining = time-1;
-        let secondsRemaining = 59;
-        timerEl.innerText = `${minutesRemaining}:${secondsRemaining}`
+        minutesRemaining<0?minutesRemaining=0:"nothing";
+        let secondsRemaining = seconds;
+        timerEl.innerText = `${this.pad(minutesRemaining)}:${this.pad(secondsRemaining)}`               
 
         const counter = setInterval(()=>{
             secondsRemaining--
@@ -29,16 +34,41 @@ const timer = {
 
             timerEl.innerText = `${this.pad(minutesRemaining)}:${this.pad(secondsRemaining)}`               
             
-        }, 100)
+        }, 1000)
     },
 
-    start: function(state, settings){
+    start(state, settings){
         switch(state){
             case 0: this.count(settings.pomodoro); break;
             case 1: this.count(settings.shortBreak); break;
             case 2: this.count(settings.longBreak); break;
         }
+    },
+
+    pause(){
+        for (var i = 1; i < 99999; i++)
+        window.clearInterval(i);
+        this.status = "pause"
+    },
+
+    resume(){
+        if(this.status==="pause"){
+            this.status = "run"
+            let time = timerEl.innerText
+            let minutesRemaining = Number(time[0]+time[1])
+            let secondsRemaining = Number(time[3]+time[4])
+            timerEl.innerText = `${this.pad(minutesRemaining)}:${this.pad(secondsRemaining)}`               
+            this.count(minutesRemaining+1, secondsRemaining-1)
+        }
     }
 }
+
+pauseBtn.addEventListener('click', ()=>{
+    timer.pause()
+})
+
+resumeBtn.addEventListener('click', ()=>{
+    timer.resume()
+})
 
 timer.start(0, settings)
